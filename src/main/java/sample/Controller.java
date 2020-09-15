5f87f9db85802675f61ab2bb781f90abc7956ce2
MC <>
2020-09-14 22:12:13 -0700

    Refine build, use custom map

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -14,6 +14,11 @@ import javafx.fxml.FXML;
 import javafx.event.Event;
 import javafx.stage.Stage;
 
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+
 public class Controller implements Initializable {
 
   @FXML
@@ -28,14 +33,38 @@ public class Controller implements Initializable {
   @FXML
   private StackPane sp;
 
+  private MapView mapView;
+
   @Override
   public void initialize (URL location, ResourceBundle resources) {
     System.out.println("View is now loaded!");
 
-    Button someButton = new Button("Some Button");
-    sp.getChildren().add(someButton);
+    //Button someButton = new Button("Some Button");
+    //sp.getChildren().add(someButton);
+
+    // create a MapView to display the map and add it to the stack pane
+    mapView = new MapView();
+    sp.getChildren().add(mapView);
+    setupMap();
+    // create an ArcGISMap with the default imagery basemap
+    //ArcGISMap map = new ArcGISMap(Basemap.createImagery());
+
+    // display the map by setting the map on the map view
+    //mapView.setMap(map);
   }
-  
+
+  private void setupMap() {
+    if (mapView != null) {
+      //Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+      Basemap.Type basemapType = Basemap.Type.DARK_GRAY_CANVAS_VECTOR;
+      double latitude = 34.02700;
+      double longitude = -118.80543;
+      int levelOfDetail = 12;
+      ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+      mapView.setMap(map);
+    }
+  }
+
   @FXML
   public void handleButtonAction (Event e) {
     System.out.println("Button clicked!");
@@ -44,8 +73,16 @@ public class Controller implements Initializable {
   @FXML
   void handleExit (Event e) {
     System.out.println("User wants to exit. Please delete your computer.");
+    dispose();
     Stage stage = (Stage) menuBar.getScene().getWindow();
     // do what you have to do
     stage.close();
   }
+
+  void dispose () {
+    if (mapView != null) {
+      mapView.dispose();
+    }
+  }
+
 }
