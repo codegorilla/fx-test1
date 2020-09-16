cdf511747013539635d0ef14c108fc0fe546ea86
MC <>
2020-09-15 22:29:42 -0700

    Enhancements
    - Rename app to GlobeCommander
    - Add graphic overlay with point graphic

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,30 +1,42 @@
 package sample;
 
-import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.scene.control.SplitPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-//import javafx.scene.control.*;
+import javafx.scene.control.SplitPane;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.event.Event;
 import javafx.stage.Stage;
+
 
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 
 import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+
+import com.esri.arcgisruntime.symbology.SimpleFillSymbol;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 
 import com.esri.arcgisruntime.mapping.view.MapView;
 
 public class Controller implements Initializable {
+
+  private int hexRed = 0xFFFF0000;
+  private int hexGreen = 0xFF00FF00;
+  private int hexBlue = 0xFF0000FF;
 
   @FXML
   private Button btnSubmit;
@@ -53,12 +65,17 @@ public class Controller implements Initializable {
   @FXML
   private Button btnZoomOut;
 
+  private GraphicsOverlay graphicsOverlay;
+
   @Override
   public void initialize (URL location, ResourceBundle resources) {
     System.out.println("View is now loaded!");
     sp.setResizableWithParent(vb, Boolean.FALSE);
     //sp.setDividerPositions(0.25);
     setupMap();
+    setupGraphicsOverlay();
+    addPointGraphic(-118.29507, 34.13501);
+    addPointGraphic(-120.5, 30.5);
   }
 
   private void setupMap() {
@@ -70,6 +87,23 @@ public class Controller implements Initializable {
       int levelOfDetail = 12;
       ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
       mapView.setMap(map);
+    }
+  }
+
+  private void setupGraphicsOverlay() {
+    if (mapView != null) {
+      graphicsOverlay = new GraphicsOverlay();
+      mapView.getGraphicsOverlays().add(graphicsOverlay);
+    }
+  }
+
+  private void addPointGraphic(double lon, double lat) {
+    if (graphicsOverlay != null) {
+      SimpleMarkerSymbol pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.SQUARE, hexRed, 10.0f);
+      pointSymbol.setOutline(new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, hexGreen, 2.0f));
+      Point point = new Point(lon, lat, SpatialReferences.getWgs84());
+      Graphic pointGraphic = new Graphic(point, pointSymbol);
+      graphicsOverlay.getGraphics().add(pointGraphic);
     }
   }
 
--- src/main/java/sample/GlobeCommander.java
+++ src/main/java/sample/GlobeCommander.java
@@ -6,7 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class GlobeCommander extends Application {
+
+  private final int START_WIDTH  = 960;
+  private final int START_HEIGHT = 540;
 
   public static void main (String[] args) {
     launch(args);
@@ -15,8 +18,8 @@ public class Main extends Application {
   @Override
   public void start (Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-    primaryStage.setTitle("Hello World");
-    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.setTitle("Globe Commander");
+    primaryStage.setScene(new Scene(root, START_WIDTH, START_HEIGHT));
     primaryStage.show();
   }
 
